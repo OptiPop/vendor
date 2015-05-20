e26ded805a97b396776a7a3a29a35db023ab74f1
Chet Kener <Cl3Kener@gmail.com>
2015-05-20 19:49:06 -0400

    Update All Products to 5.1.1 LMY48B
    Change-Id: I47ae6047c0d514f579026bfaa4dad922975ef15e
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- products/optipop_manta.mk
+++ products/optipop_manta.mk
@@ -26,7 +26,7 @@ PRODUCT_NAME := optipop_manta
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="manta" BUILD_FINGERPRINT="google/mantaray/manta:5.1.1/LMY47X/1849464:user/release-keys" PRIVATE_BUILD_DESC="mantaray-user 5.1.1 LMY47X 1849464 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME="manta" BUILD_FINGERPRINT="google/mantaray/manta:5.1.1/LMY48B/1863243:user/release-keys" PRIVATE_BUILD_DESC="mantaray-user 5.1.1 LMY48B 1863243 release-keys"
 
 # Kernel inline build
 TARGET_KERNEL_SOURCE := kernel/samsung/manta
--- products/optipop_shamu.mk
+++ products/optipop_shamu.mk
@@ -23,7 +23,7 @@ PRODUCT_NAME := optipop_shamu
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT="google/shamu/shamu:5.1.1/LMY47X/1849464:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 5.1.1 LMY47X 1849464 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=shamu BUILD_FINGERPRINT="google/shamu/shamu:5.1.1/LMY48B/1863243:user/release-keys" PRIVATE_BUILD_DESC="shamu-user 5.1.1 LMY48B 1863243 release-keys"
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
